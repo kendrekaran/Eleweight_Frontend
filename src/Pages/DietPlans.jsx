85109c170158177798b5f31de17963e5875c9e05
Karan Kendre <142140048+kendrekaran@users.noreply.github.com>
2025-03-03 13:03:35 +0530

    Update DietPlans.jsx Gemini Model

--- src/Pages/DietPlans.jsx
+++ src/Pages/DietPlans.jsx
@@ -8,7 +8,7 @@ import { Navigate } from 'react-router-dom';
 
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const FitnessApp = () => {
   const [formData, setFormData] = useState({
